70d75cae016b3a545a40a14a17a6e5d2531db64e
Daniel Ortiz <danielfoc@protonmail.com>
2022-01-05 21:43:43 -0500

    Add main header.

---
+++ inc/network.h
@@ -0,0 +1,17 @@
+/**
+ * @file network.h
+ * @author Daniel Felipe
+ * @date 03/01/2021
+ * @brief
+ */
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <vector>
+#include <utility>
+#include <string>
+
+void read_network(std::vector<std::pair<std::string, std::string> > &nodes, std::vector<std::pair<std::string, std::string> > &links);
+
+#endif
